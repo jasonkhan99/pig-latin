0a8bb1bc04537c546dadec9385f4ffec401970bb
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-10 13:30:21 -0700

    add array split method, save to variable add for loop and function to BL.
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 // Business logic
 function pigLatinTranslator(){
-
-}
+  for (let i=0; splitSentence.length > i; ++i) {
+    if splitSentence[i]
+  };
+};
 
 // User interface
 $(document).ready(function(){
   $("form#pig-latin").submit(function(event){
     event.preventDefault();
-    let inputSentance = $("input#input-sentence").val();
+    let inputSentence = $("input#input-sentence").val();
+    let splitSentence = inputSentence.split(" ");
+    console.log(splitSentence);
   });
 });
+
